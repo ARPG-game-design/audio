e96ff26c66ce7e54b21fb8fa0f73fc6b6ad9fb9f
nn0629 <50348768+nn0629@users.noreply.github.com>
2021-03-17 23:18:22 +0800

    Add files via upload

---
+++ AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour  
+{
+    public AudioMixer audioMixer;  //控制Mixer變量
+    private bool isPlayMusic;  //是否播放背景音樂
+
+    public void SetMasterVolume(float volume)  //控制主因量
+    {
+        audioMixer.SetFloat("MasterVolume", volume);  //MasterVolumes是Master暴露出的參數
+     
+    }
+
+    public void SetMusicVolume(float volume)    //控制背景音樂
+    {
+        audioMixer.SetFloat("MusicVolume", volume);  //MusicVolume是Music暴露出的參數
+
+    }
+
+
+}
---
+++ GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //使用SceneManager.LoadScene來切換場景需要添加
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pauseMenu; //暫停選單物件
+    public GameObject myBag; //背包
+    public Canvas canvas_HUD;
+   
+    private bool isPaused = false; //條件判斷是否暫停遊戲，預設不是暫停狀態
+    private bool isOpenBag = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)    //判斷按下ESC鍵時，選單是否開啟狀態來執行暫停或繼續遊戲動作 
+            PauseGame();
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
+            ContinueGame();
+        OpenMybag();
+    }
+
+    public void ContinueGame()
+    {
+        isPaused = false; 
+        pauseMenu.SetActive(false);  //關閉離開選單
+        Time.timeScale = 1; //時間繼續
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true); //開啟離開選單
+        Time.timeScale = 0; //暫停遊戲
+    }
+
+
+    public void StartGame(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName); //讀取場景
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); //結束遊戲
+    }
+
+
+    void OpenMybag()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            isOpenBag = !isOpenBag; //改變false, true
+            myBag.SetActive(isOpenBag);
+        }
+        
+    }
+
+    public void CloseMybagButton()
+    {
+        isOpenBag = !isOpenBag; //改變false, true
+        myBag.SetActive(isOpenBag);
+    }
+}   
